77c51ee61d95c0f370119c083580bdc416ee4c03
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-10-17 17:45:09 +0300

    printing is good now but row 1 is zeros for some reason but when assigning values its not and idk how to remove printing row 0 also getting the values of one row is working well by giving the needed row

--- Lab5/GraphMap.py
+++ Lab5/GraphMap.py
@@ -20,19 +20,31 @@ class Graph:
             return False
 
     def add_edge(self, u, v, weight):
+
         if u in self.vertices and v in self.vertices:
             self.edges[self.edge_indices[u]][self.edge_indices[v]] = weight
+            #print(u , v , " cost is " ,weight)
             self.edges[self.edge_indices[v]][self.edge_indices[u]] = weight
             return True
         else:
             return False
 
     def print_graph(self):
+        for i in range(len(self.edges)):
+            print(i, " ", end="")
+        print("")
         for v in range(len(self.edges)):
-            print(v, " ", end=" ")
+            print(v, "", end=" ")
             for j in range(len(self.edges)):
-                print(self.edges[i][j], end="")
-            print(" ")
+                print(" ", self.edges[v][j], end="")
+            print("")
+
+    def oneRowDicretion(self, row):
+        count = 0
+        for i in range(len(self.edges)):
+            count += self.edges[row][i]
+
+        return count
 
 
 g = Graph()
@@ -46,35 +58,35 @@ for i in range(1, 20):
 
 edges = [
     # edge one , edge two , cost between them
-    [20,1,3],
-    [20,16,2],
-    [1,17,0.5],
-    [1,9,3],
-    [1,2,2],
-    [16,17,0.3],
-    [10,8,1],
-    [17,8,0.5],
-    [2,18,0.5],
-    [2,3,1.5],
-    [3,9,2],
-    [3,10,2],
-    [3,4,0.3],
-    [4,11,1],
-    [4,5,0.5],
-    [5,11,2],
-    [5,12,0.5],
-    [5,6,0.2],
-    [6,7,0.3],
-    [6,13,0.4],
-    [7,13,0.2],
-    [7,14,0.3],
-    [14,13,0.1],
-    [14,12,0.1],
-    [12,15,0.5]
+    [20, 1, 3],
+    [20, 16, 2],
+    [1, 17, 0.5],
+    [1, 9, 3],
+    [1, 2, 2],
+    [16, 17, 0.3],
+    [10, 8, 1],
+    [17, 8, 0.5],
+    [2, 18, 0.5],
+    [2, 3, 1.5],
+    [3, 9, 2],
+    [3, 10, 2],
+    [3, 4, 0.3],
+    [4, 11, 1],
+    [4, 5, 0.5],
+    [5, 11, 2],
+    [5, 12, 0.5],
+    [5, 6, 0.2],
+    [6, 7, 0.3],
+    [6, 13, 0.4],
+    [7, 13, 0.2],
+    [7, 14, 0.3],
+    [14, 13, 0.1],
+    [14, 12, 0.1],
+    [12, 15, 0.5]
 ]
 
 for edge in edges:
-    g.add_edge(edge[0] ,edge[1] ,edge[2])
+    g.add_edge(edge[0], edge[1], edge[2])
 
-
+print(g.oneRowDicretion(2))
 g.print_graph()
