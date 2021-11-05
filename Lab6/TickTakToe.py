788b3db71b41f30905a469d84eb5f6cc6ea336db
khalil alsulaimani <alsulaimani.2000@outlook.com>
2021-11-05 20:43:34 +0300

    lab 6 fully working has ablity for playing against AI first or second or against a friend

---
+++ Lab6/TickTakToe.py
@@ -0,0 +1,176 @@
+def printBoard(board):
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print("\n")
+
+
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if (checkDraw()):
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print("Bot wins!")
+                exit()
+            else:
+                print("Player wins!")
+                exit()
+
+        return
+
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+
+def playerOneMove():
+    position = int(input("Enter the position for 'X':  "))
+    insertLetter(bot, position)
+    return
+
+
+def compMove():
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+
+    insertLetter(bot, bestMove)
+    return
+
+
+def minimax(board, depth, isMaximizing):
+    if (checkWhichMarkWon(bot)):
+        return 1
+    elif (checkWhichMarkWon(player)):
+        return -1
+    elif (checkDraw()):
+        return 0
+
+    if (isMaximizing):
+        bestScore = -800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = bot
+                score = minimax(board, depth + 1, False)
+                board[key] = ' '
+                if (score > bestScore):
+                    bestScore = score
+        return bestScore
+
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = player
+                score = minimax(board, depth + 1, True)
+                board[key] = ' '
+                if (score < bestScore):
+                    bestScore = score
+        return bestScore
+
+
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+player = 'O'
+bot = 'X'
+whoPlays = input("Do you Want to Play Against AI or Friend : ")
+
+while True:
+    if whoPlays.lower() == "ai":
+        whosFirst = input("Do You Want To Play first :")
+        if whosFirst.lower() == "yes" or whosFirst == "y":
+            printBoard(board)
+            print("enter positions from 1 till 9 to put your mark on the board")
+            while not checkForWin():
+                playerMove()
+                compMove()
+        else:
+            while not checkForWin():
+                compMove()
+                playerMove()
+    elif whoPlays.lower() == "friend":
+        while not checkForWin():
+            playerOneMove()
+            playerMove()
+
+    else:
+        whoPlays = input("invalid option please try again enter AI or Friend in any case : ")
